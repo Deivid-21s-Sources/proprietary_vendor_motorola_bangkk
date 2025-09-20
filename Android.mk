8e5e6baae2094273a33d2015fb1ef5bbe5b3146c
Deivid21 <david.parra.ignacio@gmail.com>
2025-09-20 01:12:37 -0300

    bangkk: Move radio to external repository * Moved to: "https://gitlab.com/Deivid-21s-Sources/proprietary_vendor_motorola_bangkk-radio.git"
    Change-Id: I4ca1b8eac7812fa442dc5b90b4ba2f6991087fa0

--- Android.mk
+++ Android.mk
@@ -1,27 +1,3 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
-
-LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),bangkk)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,f9479f031fc2bb840739c65c4636628fbc1a1489)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,19e9c9025185d31b629ad93ad3a727372fc7bd5e)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,8bfd8ca3edb11dc52bfd24936fac66a2eddf5c99)
-$(call add-radio-file-sha1-checked,radio/dsp.img,6be03a523b4a75f993c9cf3f14537feaf6f2e57e)
-$(call add-radio-file-sha1-checked,radio/fsg.img,25e5eef5575f2f8b2c49d595b4f488857b38d383)
-$(call add-radio-file-sha1-checked,radio/hyp.img,8a68253096a1dbc87a17668511b5140e9d86e7ac)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,765ef277245c7e0777a980adbaac479df509bcb6)
-$(call add-radio-file-sha1-checked,radio/logo.img,8cf454edbd3d21b817eccfc404498304bf7e906a)
-$(call add-radio-file-sha1-checked,radio/modem.img,4e922c20084d15284c7d825d6662c8fb8a203a10)
-$(call add-radio-file-sha1-checked,radio/prov.img,739ae636f006727f198a856ec1f932efb066a3e3)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,9817e9f4eab356be66eb74dbb770afc1b781e189)
-$(call add-radio-file-sha1-checked,radio/rpm.img,c69e347489dbd30a7a2133023fdb3a64d862ac0c)
-$(call add-radio-file-sha1-checked,radio/storsec.img,99d9d9c089c4f3ff6b08c301098b975e5a646170)
-$(call add-radio-file-sha1-checked,radio/tz.img,d4a36a917ea55faed5409be915784b984f34a846)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,9e4b93320a992455e8fd7d5a158e3a35be44e39f)
-$(call add-radio-file-sha1-checked,radio/xbl.img,a8aefd9f05ec10647c7e95b249d991d8c75fb5c7)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,fa460fc8d937ce5c25e1e100413ef32a8ef9593d)
-
-endif
